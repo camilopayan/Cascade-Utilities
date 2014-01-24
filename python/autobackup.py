7624600ba3c6ae312ff8824d509ae8438a69dc63
Camilo Payan <cpayan@gmail.com>
2014-01-23 23:50:55 -0500

    Adds time to the filename. Still needs to actually work the mysqldump into there though

--- python/autobackup.py
+++ python/autobackup.py
@@ -1,11 +1,21 @@
 import os
 import subprocess
+from datetime import date
 
-cascade_folder = "/usr/local/Cascade\ Server"
+cascade_folder      = "/usr/local/Cascade\ Server"
+cascade_database    = "cascade"
+cascade_db_user     = "CASCADE_USER"
+cascade_db_passwd   = "CASCADE_PASSWORD"
+today               = date.today()
 
 os.chdir(cascade_folder)
 
 cascade_shutdown    = subprocess.Popen("./cascade.sh stop", shell=True)
 cascade_shutdown.wait()
-print "Cascade shut down with return status " + cascade_shutdown.return_code
+print "Cascade has shut down"
+
+dump_output         = open("cascade_backup_" + str(today.year) + str(today.month) +
+        str(day) + ".bkp.gz", "wb")
+
 cascade_startup     = subprocess.Popen("./cascade.sh start")
+print "Cascade has restarted"
